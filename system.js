e210705b14d75775a403ce1b7426624b9b781033
James Rutledge <jarutledge2@gmail.com>
2016-03-31 21:04:45 -0400

    achievements added

--- system.js
+++ system.js
@@ -1,23 +1,34 @@
 var Engine = { //the main Engine object
 	/** variables **/
 	Player: { //the player object
+		Clicks: 0, //how many clicks
 		Coins: 0, //how many coins that player has
 		PerClick: 1, //how many coins per click
-		PerIncrement: 1, //how many coins per timed increment
+		PerIncrement: 0, //how many coins per timed increment
 		Increment: 2000 //how long the increment is (2secs)
 	},
+	StatusMessage: "",
 	Canvas: { //the canvas object
 		Element: null, //this will be a canvas element
 		Context: null //this will become a 2d context
 	},
-	Timers: {
-		Increment: null,
+	Timers: { //this holds all sorts of timers
+		Increment: null, //this is the main coin increment,
+		StatusMessage: null, //this is so we can reset the status message timer
 	},
-
-	Elements: {
-		ClickBox: {
-			x: 320, y: 180,
-			w: 128, h: 128
+	
+	/** achievements **/
+	Achievements: [ //the achievements array (name, how many clicks to earn and the reward)
+		{ Name: "Small Clicker", Clicks: 25, Reward: 0 },
+		{ Name: "Medium Clicker", Clicks: 50, Reward: 0 },
+		{ Name: "Large Clicker", Clicks: 100, Reward: 50 }
+	],
+	
+	/** elements **/
+	Elements: { //this holds the elements
+		ClickBox: { //this is our main click element
+			x: 320, y: 180, //position
+			w: 128, h: 128 //size
 		}
 	},
 	
@@ -28,23 +39,43 @@ var Engine = { //the main Engine object
 		Engine.Canvas.width = 800; //the width
 		Engine.Canvas.height = 600; //the height
 		$('body').append(Engine.Canvas); //finally append the canvas to the page
-
-		Engine.AddClick();
-		Engine.StartIncrement();
+		
+		Engine.AddClick(); //start the main click event
+		Engine.StartIncrement(); //start the auto coins		
 		Engine.Canvas.Context = Engine.Canvas.getContext('2d'); //set the canvas to render in 2d.
 		Engine.GameLoop(); //start rendering the game!
+		
 	},
-
-	StartIncrement: function() {
-		Engine.Timers.Increment = setInterval(function() {
-			Engine.Player.Coins += Engine.Player.PerIncrement;
-		}, Engine.Player.Increment);
+	IncreaseCoins: function(amount) { //the new coin adding function
+		Engine.Player.Coins += amount; //increase coins by amount
+		Engine.CheckAchievements(); //check achievements
+	},
+	CheckAchievements: function() {
+		for (var a = 0; a < Engine.Achievements.length; a++) { //loop through each achievement in the array
+			if (Engine.Achievements[a].Clicks == Engine.Player.Clicks) { //have you matched the achievement clicks?
+				Engine.StatusMessage = "GOT ACHIEVEMENT: " + Engine.Achievements[a].Name; //show message with achievement name
+				clearTimeout(Engine.Timers.StatusMessage); //clear the current timeout
+				Engine.Timers.StatusMessage = setTimeout(function() { //assign a new one
+					Engine.StatusMessage = ""; //reset message after 3 seconds
+					clearTimeout(Engine.Timers.StatusMessage);
+					Engine.Timers.StatusMessage = null;
+				}, 3000); //3 secs
+			}
+		}
+	},
+	
+	/** event handlers **/
+	StartIncrement: function() { //automatic coins
+		Engine.Timers.Increment = setInterval(function() { //set the Timer as an interval
+			Engine.IncreaseCoins(Engine.Player.PerIncrement); //call coin increase
+		}, Engine.Player.Increment); //set to the players increment time
 	},
 	AddClick: function() { //the click function
 		$(Engine.Canvas).on('click', function(m) { //we add a click to the Engine.Canvas object (note the 'm')
 			if (m.pageX >= Engine.Elements.ClickBox.x && m.pageX <= (Engine.Elements.ClickBox.x + Engine.Elements.ClickBox.w)) { //check to see if the click is within the box X co-ordinates
 				if (m.pageY >= Engine.Elements.ClickBox.y && m.pageY <= (Engine.Elements.ClickBox.y + Engine.Elements.ClickBox.h)) { //check to see if the click is within the box Y co-ordinates
-					Engine.Player.Coins += Engine.Player.PerClick; //increase the coins by the PerClick amount!
+					Engine.Player.Clicks++; //add a click
+					Engine.IncreaseCoins(Engine.Player.PerClick); //call coin increase
 				}
 			}
 			return false;
@@ -54,18 +85,22 @@ var Engine = { //the main Engine object
 	/** animation routines **/
 	GameRunning: null, //this is a new variable so we can pause/stop the game
 	Update: function() { //this is where our logic gets updated
+		
 		Engine.Draw(); //call the canvas draw function
 	},
 	Draw: function() { //this is where we will draw all the information for the game!
 		Engine.Canvas.Context.clearRect(0,0,Engine.Canvas.width,Engine.Canvas.height); //clear the frame
-
-		Engine.Rect(320, 180, 128, 128, "silver"); //click button
+		
+		/** click button! **/
+		Engine.Rect(Engine.Elements.ClickBox.x, Engine.Elements.ClickBox.y, Engine.Elements.ClickBox.w, Engine.Elements.ClickBox.h, "silver"); //use the Rect function to draw the click button!
 		Engine.Text("CLICK ME", 330, 210, "Calibri", 28, "orange"); //click button text
-
+		
+		/** display/hud **/
 		Engine.Text(Engine.Player.Coins + " Coins", 16, 32, "Calibri", 20, "blue"); //coin display
 		Engine.Text(Engine.Player.PerClick + " Coins per click", 16, 56, "Calibri", 20, "blue"); //per click display
 		Engine.Text(Engine.Player.PerIncrement + " Coins every " + (Engine.Player.Increment / 1000) + "secs", 16, 80, "Calibri", 20, "blue"); //increment display
-
+		Engine.Text(Engine.StatusMessage, 16, 104, "Calibri", 20, "orange"); //new status message
+		
 		Engine.GameLoop(); //re-iterate back to gameloop
 	},
 	GameLoop: function() { //the gameloop function
